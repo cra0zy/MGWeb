f4d4cc43aa406f9a54123c70da27acc72eb74a2d
cra0zy <cra0zy@gmail.com>
2015-11-05 14:22:18 +0100

    Initial commit

---
+++ MGWeb/Game1.cs
@@ -0,0 +1,57 @@
+﻿using System;
+
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Input;
+
+namespace MGWeb
+{
+    public class Game1 : Game
+    {
+        GraphicsDeviceManager graphics;
+        KeyboardState pkstate;
+
+        public Game1()
+        {
+            graphics = new GraphicsDeviceManager(this);
+        }
+
+        protected override void Update(GameTime gameTime)
+        {
+            var kstate = Keyboard.GetState();
+            var jstate = Joystick.GetState(0);
+
+            if (kstate.IsKeyDown(Keys.A) && pkstate.IsKeyUp(Keys.A))
+                Console.WriteLine("A is pressed :D");
+
+            if (kstate.IsKeyDown(Keys.Z) && pkstate.IsKeyUp(Keys.Z))
+                Console.WriteLine("Z is pressed :D");
+
+            if (kstate.IsKeyDown(Keys.M) && pkstate.IsKeyUp(Keys.M))
+                Console.WriteLine("Mouse position: " + Mouse.GetState().Position.ToString());
+
+            if (kstate.IsKeyDown(Keys.L) && pkstate.IsKeyUp(Keys.L))
+                Console.WriteLine("Left mouse button: " + Mouse.GetState().LeftButton);
+
+            if (jstate.IsConnected && kstate.IsKeyDown(Keys.J) && pkstate.IsKeyUp(Keys.J))
+                Console.WriteLine("Joystick: " + Joystick.GetCapabilities(0).Id + " state:" + jstate.Axes[0] + " " + jstate.Buttons[0]);
+
+            if (jstate.IsConnected && kstate.IsKeyDown(Keys.G) && pkstate.IsKeyUp(Keys.G))
+            {
+                var gstate = GamePad.GetState(0);
+                Console.WriteLine("Gamepad: A:" + gstate.IsButtonDown(Buttons.A) + " B: " + gstate.IsButtonDown(Buttons.B) + " Dpad down: " + gstate.DPad.Down + " Left Trigger " + gstate.Triggers.Left + " R Axis: " + gstate.ThumbSticks.Right.ToString());
+            }
+
+            pkstate = kstate;
+
+            base.Update(gameTime);
+        }
+
+        protected override void Draw(GameTime gameTime)
+        {
+            graphics.GraphicsDevice.Clear(Color.CornflowerBlue);
+            
+            base.Draw(gameTime);
+        }
+    }
+}
+
---
+++ MGWeb/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace MGWeb
+{
+    static class Program
+    {
+        private static Game1 game;
+
+		static void Main(string[] args)
+        {
+            game = new Game1();
+            game.Run();
+        }
+    }
+}
+
